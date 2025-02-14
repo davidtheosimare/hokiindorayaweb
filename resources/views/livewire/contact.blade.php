5f010581c8f761d7c77f87cd724f9318eec17944
David Theo <davidtheosimaremare@gmail.com>
2025-02-14 09:57:39 +0700

    update meta seo

--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -1,3 +1,23 @@
+@section('meta')
+<meta name="description" content="{{$description}}">
+<meta name="keywords" content="{{$keywords}}">
+<meta name="robots" content="index, follow">
+
+ <!-- Open Graph Tags untuk Facebook dan WhatsApp -->
+ <meta property="og:title" content="{{$title}}">
+ <meta property="og:description" content="{{$description}}">
+ <meta property="og:image" content="{{$image}}">
+ <meta property="og:url" content="{{$url}}">
+ <meta property="og:type" content="website">
+
+ <!-- Twitter Card Tags -->
+ <meta name="twitter:card" content="summary_large_image">
+ <meta name="twitter:title" content="{{$title}}">
+ <meta name="twitter:description" content="{{$description}}">
+ <meta name="twitter:image" content="{{$image}}">
+ <title>{{$title ?? 'Hokiindo Raya'}} - {{$description}}</title>
+
+@endsection
 <div>
     <section class="p-0 overflow-hidden top-space-margin bg-very-light-gray page-title-big-typography">
         <div class="p-0 container-fluid">
